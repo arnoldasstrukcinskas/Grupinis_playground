fb7e2962daf5ad617b49249e694eef5753781ec8
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2026-05-28 12:53:21 +0300

    Initial commit

---
+++ src/main/java/lt/viko/eif/astrukcinskas/grupinis_playground/GrupinisPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.grupinis_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupinisPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrupinisPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/viko/eif/astrukcinskas/grupinis_playground/GrupinisPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.grupinis_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupinisPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
